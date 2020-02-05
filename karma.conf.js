7503dffd19c163b7e67f0a913e7dac3a14649b7a
InduSekhar Velagapudi <InduSekhar_Velagapudi@ao.uscourts.gov>
2020-02-04 20:12:52 -0500

    Karma conf change

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const process = require('process'); process.env.CHROME_BIN = 
+require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -10,7 +12,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-phantomjs-launcher'),
+      
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -25,8 +29,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
