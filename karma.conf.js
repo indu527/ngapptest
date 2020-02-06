bfe8d7d36342c9286b3569d924b7b74188ed43d5
InduSekhar Velagapudi <InduSekhar_Velagapudi@ao.uscourts.gov>
2020-02-05 19:55:35 -0500

    Karma karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const process = require('process'); process.env.CHROME_BIN = 
-require('puppeteer').executablePath();
+// const process = require('process'); process.env.CHROME_BIN = 
+// require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -29,7 +29,19 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['PhantomJS'],
+    // customLaunchers: {
+    //   ChromeHeadless: {
+    //     base: 'Chrome',
+    //     flags: [
+    //       '--headless',
+    //       '--disable-gpu',
+    //       '--no-sandbox',
+    //       '--remote-debugging-port=9222',
+    //     ]
+    //   }
+    // },
+
     singleRun: true,
     restartOnFileChange: true
   });
